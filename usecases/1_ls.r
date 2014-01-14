fd9cb2445b0a409e8d88e763d4413f5c5cdb4f49
Tim Poisot <timothee.poisot@uqar.ca>
2014-01-14 12:02:41 -0500

    Figure 1

--- usecases/1_ls.r
+++ usecases/1_ls.r
@@ -16,5 +16,13 @@ ls$X1 <- aaply(as.numeric(as.vector(ls$X1)), 1, function(x) getNetwork(api, x)$n
 colnames(ls)[1] <- 'Network'
 
 # Now plot this dataset
-plot(jitter(L)~jitter(S), ls, log='xy', pch=22, bg='lightgrey', lwd=1.5, cex=1.5)
-
+plot(jitter(L)~jitter(S), ls, log='xy', pch=22, bg='lightgrey', lwd=1.5, cex=1.5, xlim=c(5, 15), xlab='Species richness', ylab='Number of interactions')
+# Constant connectance
+X <- c(1:15)
+Y <- X^2 * mean(ls$L/ls$S^2)
+lines(X, Y, lty=2)
+# Best fit
+bfit <- lm(L~S, ls)
+Yf <- X * bfit$coefficients[2] + bfit$coefficients[1]
+lines(X, Yf)
+legend('bottomright',pch=c(22, NA, NA), lty=c(NA, 2, 1), pt.cex=c(1.5, 1, 1), lwd=c(1.5, 1, 1), pt.bg=c('lightgrey', NA, NA), legend=c('Data', 'Constant connectance', 'Best fit (linear model)'), bty='n')
